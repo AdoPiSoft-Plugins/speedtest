146530d279c5075097d7f2214b8eab497316d87c
Arnel Lenteria <arnel.lenteria@gmail.com>
2020-09-18 17:42:43 +0800

    add middlewares

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 'use strict'
 
 var core = require('../core')
-var { router } = core
+var { router, middlewares } = core
+var { express, bodyParser } = middlewares
 var speedtest_ctrl = require('./controllers/speedtest_ctrl')
 
 router.get('/speedtest-plugin', speedtest_ctrl.get)
-router.post('/speedtest-plugin/start', speedtest_ctrl.start)
+router.post('/speedtest-plugin/start', express.urlencoded({ extended: true }), bodyParser.json(), speedtest_ctrl.start)
 
 module.exports = router

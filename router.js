934711ba45783dd9d6081dff49aca8c5172163b2
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-07-23 14:26:29 +0800

    ecd1a570b7a6572b031719c2011d1720b4de7fe1

--- router.js
+++ router.js
@@ -1,9 +1,1 @@
-var core = require('plugin-core')
-var { router, middlewares } = core
-var { express, bodyParser } = middlewares
-var speedtest_ctrl = require('./controllers/speedtest_ctrl')
-
-router.get('/speedtest-plugin', speedtest_ctrl.get)
-router.post('/speedtest-plugin/start', express.urlencoded({ extended: true }), bodyParser.json(), speedtest_ctrl.start)
-
-module.exports = router
+var core=require("plugin-core");var{router,middlewares}=core;var{express,bodyParser}=middlewares;var speedtest_ctrl=require("./controllers/speedtest_ctrl");router.get("/speedtest-plugin",speedtest_ctrl.get);router.post("/speedtest-plugin/start",express.urlencoded({extended:true}),bodyParser.json(),speedtest_ctrl.start);module.exports=router;
